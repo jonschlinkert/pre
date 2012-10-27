7e7732ea4c791f84a2be4a4715114399cae73e14
Jon Schlinkert <github@sellside.com>
2012-10-27 19:17:53 -0400

    - moved grunt tasks into build folder - updated attribution in wrap task to give credit to Dmitry Pashkevich (http://stackoverflow.com/users/1033615/dmitry-pashkevich)

--- build/wrap.js
+++ build/wrap.js
@@ -1,14 +1,20 @@
+/*
+ * Copyright (c) 2012 Dmitry Pashkevich (http://stackoverflow.com/users/1033615/dmitry-pashkevich)
+ * Licensed under the MIT license.
+ * https://github.com/gruntjs/grunt/blob/master/LICENSE-MIT
+ */
+
 module.exports = function( grunt ) {
 'use strict';
 
-  grunt.registerMultiTask('append', 'Appends and prepends source files with specified header and footer', function() {
+  grunt.registerMultiTask('wrap', 'Wraps source files with specified header and footer', function() {
     var data = this.data,
         path = require('path'),
         dest = grunt.template.process(data.dest),
         files = grunt.file.expandFiles(this.file.src),
         header = grunt.file.read(grunt.template.process(data.header)),
         footer = grunt.file.read(grunt.template.process(data.footer)),
-        sep = grunt.utils.linefeed; 
+        sep = grunt.utils.linefeed;
 
     files.forEach(function(f) {
         var p = dest + '/' + path.basename(f),
